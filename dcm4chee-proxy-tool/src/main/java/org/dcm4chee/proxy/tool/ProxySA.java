f781380c78507ff9308885f1432f1a02c0aa13de
Michael Backhaus <michael.backhaus@agfa.com>
2012-01-12 17:22:20 +0100

    added config audit log interval as command line option

--- dcm4chee-proxy-tool/src/main/java/org/dcm4chee/proxy/tool/ProxySA.java
+++ dcm4chee-proxy-tool/src/main/java/org/dcm4chee/proxy/tool/ProxySA.java
@@ -1,3 +1,41 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 package org.dcm4chee.proxy.tool;
 
 import java.util.ResourceBundle;
@@ -29,11 +67,14 @@ import org.dcm4chee.proxy.mc.net.Scheduler;
 import org.dcm4chee.proxy.mc.net.service.CEchoSCPImpl;
 import org.dcm4chee.proxy.mc.net.service.CStoreSCPImpl;
 
+/**
+ * @author Michael Backhaus <michael.backhaus@agfa.com>
+ */
 public class ProxySA {
     
     private static ResourceBundle rb =
         ResourceBundle.getBundle("org.dcm4chee.proxy.tool.messages");
-
+    
     public static void main(String[] args) {
         try {
             CommandLine cl = parseComandLine(args);
@@ -78,13 +119,16 @@ public class ProxySA {
         return dicomConfig;
     }
     
-    private static Scheduler configureScheduler(CommandLine cl, ProxyDevice proxyDevice) {
+    private static void configureScheduler(CommandLine cl, ProxyDevice proxyDevice) {
         AuditLog log = new AuditLog();
         if(cl.hasOption("audit-log-delay"))
             log.setDelay(Long.parseLong(cl.getOptionValue("audit-log-delay")));
         else
             log.setDelay(60);
-        return new Scheduler(proxyDevice, log);
+        Scheduler scheduler = new Scheduler(proxyDevice, log);
+        if(cl.hasOption("log-interval"))
+            proxyDevice.setSchedulerInterval(Integer.parseInt(cl.getOptionValue("log-interval")));
+        scheduler.start();
     }
 
     private static void configureKeyManager(CommandLine cl, ProxyDevice proxyDevice) {
@@ -110,6 +154,7 @@ public class ProxySA {
         addCommonOptions(opts);
         addTLSOptions(opts);
         addLDAPOptions(opts);
+        addAuditLogOptions(opts);
         return parseComandLine(args, opts, rb, ProxySA.class);
     }
     
@@ -166,6 +211,16 @@ public class ProxySA {
                 .withArgName("password")
                 .withDescription(rb.getString("key-pwd"))
                 .withLongOpt("key-pwd")
+                .create(null));
+    }
+    
+    @SuppressWarnings("static-access")
+    private static void addAuditLogOptions(Options opts) {
+        opts.addOption(OptionBuilder
+                .hasArg()
+                .withArgName("integer")
+                .withDescription(rb.getString("log-interval"))
+                .withLongOpt("log-interval")
                 .create(null));
     }
     
