be85f8baff14e36a71774e5ba2682a03b7a5d813
Michael Backhaus <michael.backhaus@agfa.com>
2012-10-25 11:58:52 +0200

    [PRX-43] add RESTful service for reload device configuration

--- dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/service/ProxyServlet.java
+++ dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/service/ProxyServlet.java
@@ -1,3 +1,41 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+ * Java(TM), hosted at https://github.com/gunterze/dcm4che.
+ *
+ * The Initial Developer of the Original Code is
+ * Agfa Healthcare.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * See @authors listed below
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 package org.dcm4chee.proxy.service;
 
 import java.lang.management.ManagementFactory;
@@ -7,25 +45,30 @@ import javax.management.ObjectName;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import org.dcm4che.conf.api.DicomConfiguration;
 
 @SuppressWarnings("serial")
+@Path("config")
+/**
+ * @author Michael Backhaus <michael.backhaus@agfa.com>
+ */
 public class ProxyServlet extends HttpServlet {
-    
+
     private ObjectInstance mbean;
-    
+
     private DicomConfiguration dicomConfig;
-    
+
     private Proxy proxy;
-    
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         try {
-            dicomConfig = (DicomConfiguration) Class.forName(
-                    config.getInitParameter("dicomConfigurationClass"), 
-                    false,
+            dicomConfig = (DicomConfiguration) Class.forName(config.getInitParameter("dicomConfigurationClass"), false,
                     Thread.currentThread().getContextClassLoader()).newInstance();
             String systemPropertyDeviceName = System.getProperty("proxy.device.name");
             String deviceName = config.getInitParameter("deviceName");
@@ -38,7 +81,7 @@ public class ProxyServlet extends HttpServlet {
             throw new ServletException(e);
         }
     }
-    
+
     @Override
     public void destroy() {
         if (mbean != null)
@@ -51,5 +94,16 @@ public class ProxyServlet extends HttpServlet {
             proxy.stop();
         if (dicomConfig != null)
             dicomConfig.close();
+    }
+
+    @POST
+    @Path("/reload/{name}")
+    public void reloadConfiguration(@PathParam("name") String name) {
+        try {
+            ManagementFactory.getPlatformMBeanServer().invoke(new ObjectName("dcm4che:service=" + name),
+                    "reloadConfiguration", null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
