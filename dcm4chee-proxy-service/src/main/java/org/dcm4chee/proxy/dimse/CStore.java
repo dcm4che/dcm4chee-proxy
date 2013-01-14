0d91f9a972d9fb77edee097a57fd9a1e24c4025d
Michael Backhaus <michael.backhaus@agfa.com>
2013-01-14 11:14:57 +0100

    [PRX-91] fix configuration exception error message output

--- dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/dimse/CStore.java
+++ dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/dimse/CStore.java
@@ -126,7 +126,7 @@ public class CStore extends BasicCStoreSCP {
             else
                 processForwardRules(as, forwardAssociationProperty, pc, rq, rsp, file, digest, fmi);
         } catch (ConfigurationException e) {
-            LOG.error(as + ": error processing C-STORE-RQ: ", e);
+            LOG.error(as + ": error processing C-STORE-RQ: " + e.getMessage());
             throw new DicomServiceException(Status.UnableToProcess);
         } finally {
             deleteFile(as, file);
