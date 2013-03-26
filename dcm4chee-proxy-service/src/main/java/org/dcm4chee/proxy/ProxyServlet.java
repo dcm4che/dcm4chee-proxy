e98c08e2ceac8b0c892365d5d523d2e7f3866794
Michael Backhaus <michael.backhaus@agfa.com>
2013-03-26 14:22:47 +0100

    [PRX-126] add startup message about proxy git version

--- dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/ProxyServlet.java
+++ dcm4chee-proxy-service/src/main/java/org/dcm4chee/proxy/ProxyServlet.java
@@ -67,6 +67,8 @@ import org.dcm4chee.proxy.conf.prefs.PreferencesProxyConfigurationExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.jcabi.manifests.Manifests;
+
 /**
  * @author Michael Backhaus <michael.backhaus@agfa.com>
  */
@@ -120,6 +122,7 @@ public class ProxyServlet extends HttpServlet {
             proxy = new Proxy(dicomConfig, hl7Config, deviceName);
             proxy.start();
             mbean = ManagementFactory.getPlatformMBeanServer().registerMBean(proxy, new ObjectName(jmxName));
+            LOG.info("Started proxy version [" + Manifests.read("Proxy-Implementation-Build") + "]");
         } catch (Exception e) {
             LOG.debug(e.getMessage(), e);
             destroy();
