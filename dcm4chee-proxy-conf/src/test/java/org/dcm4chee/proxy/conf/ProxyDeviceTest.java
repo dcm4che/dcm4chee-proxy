449970ec5ffff467a07a49ee34a3fef2c1f89fbb
Hesham Elbadawi <bsdreko@gmail.com>
2014-05-20 13:04:47 +0200

    Fix Test after library changes

--- dcm4chee-proxy-conf/src/test/java/org/dcm4chee/proxy/conf/ProxyDeviceTest.java
+++ dcm4chee-proxy-conf/src/test/java/org/dcm4chee/proxy/conf/ProxyDeviceTest.java
@@ -127,13 +127,13 @@ public class ProxyDeviceTest {
     private static final Code INST_A = new Code("111.1111", "99DCM4CHEE", null, "Site A");
 
     private static final String[] IMAGE_TSUIDS = { UID.ImplicitVRLittleEndian, UID.ExplicitVRLittleEndian,
-            UID.DeflatedExplicitVRLittleEndian, UID.ExplicitVRBigEndian, UID.JPEGBaseline1, UID.JPEGExtended24,
+            UID.DeflatedExplicitVRLittleEndian, UID.JPEGBaseline1, UID.JPEGExtended24,
             UID.JPEGLossless, UID.JPEGLosslessNonHierarchical14, UID.JPEGLSLossless, UID.JPEGLSLossyNearLossless,
             UID.JPEG2000LosslessOnly, UID.JPEG2000, UID.RLELossless };
     private static final String[] VIDEO_TSUIDS = { UID.JPEGBaseline1, UID.MPEG2, UID.MPEG2MainProfileHighLevel,
             UID.MPEG4AVCH264BDCompatibleHighProfileLevel41, UID.MPEG4AVCH264HighProfileLevel41 };
     private static final String[] OTHER_TSUIDS = { UID.ImplicitVRLittleEndian, UID.ExplicitVRLittleEndian,
-            UID.DeflatedExplicitVRLittleEndian, UID.ExplicitVRBigEndian, };
+            UID.DeflatedExplicitVRLittleEndian, };
     private static final String[] IMAGE_CUIDS = { UID.ComputedRadiographyImageStorage,
             UID.DigitalXRayImageStorageForPresentation, UID.DigitalXRayImageStorageForProcessing,
             UID.DigitalMammographyXRayImageStorageForPresentation, UID.DigitalMammographyXRayImageStorageForProcessing,
